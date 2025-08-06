65d96d5d1311893db182f12e49cc3e26117ded39
PilkeySEK <PilkeySEK@gmx.de>
2025-08-06 14:30:52 +0200

    add functions in database.ts

--- server/src/api/Database.ts
+++ server/src/api/Database.ts
@@ -14,10 +14,27 @@ export async function createUser(user: User): Promise<DatabaseOperationResult> {
   return res.acknowledged;
 }
 
+export async function deleteUserByID(user_id: string): Promise<DatabaseOperationResult> {
+  const res = await users.deleteOne({user_id: user_id});
+  return res.acknowledged;
+}
+
 export async function userWithUsernameExists(
   username: string,
 ): Promise<boolean> {
-  const findRes = await users.findOne({ username: username });
-  if (findRes === null) return false;
+  const res = await users.findOne({ username: username });
+  if (res === null) return false;
   return true;
+}
+
+export async function userWithIDExists(user_id: string): Promise<boolean> {
+  const res = await users.findOne({user_id: user_id});
+  if(res === null) return false;
+  return true;
+}
+
+export async function getUserByID(user_id: string): Promise<User | undefined> {
+  const res = await users.findOne({user_id: user_id});
+  if(res === null) return undefined;
+  return res;
 }
