6f41a7e35f0da3f4bfbb9dca60729f7bb80d035f
PilkeySEK <PilkeySEK@gmx.de>
2025-08-06 13:40:55 +0200

    connect to mongo

--- server/src/api/Database.ts
+++ server/src/api/Database.ts
@@ -1,6 +1,13 @@
 import { User } from "./DatabaseSchemas";
+import { mongo_uri } from "../../../config.json";
+import { MongoClient } from "mongodb";
 
 type DatabaseOperationResult = boolean;
+
+const client = new MongoClient(mongo_uri);
+
+const database = client.db("skyenet-expansion");
+const users = database.collection<User>("users");
 
 export async function createUser(user: User): Promise<DatabaseOperationResult> {
   // TODO
