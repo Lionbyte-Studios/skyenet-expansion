b7ea0a8e2a0ed10858571d945d780daa5c9f280a
PilkeySEK <PilkeySEK@gmx.de>
2025-09-30 19:43:49 +0200

    add _ in front of 'info'

--- client/src/graphics/screen/ShipSelectionScreen.ts
+++ client/src/graphics/screen/ShipSelectionScreen.ts
@@ -75,7 +75,7 @@ export class ShipSelectionScreen extends ClientScreen {
     }
   }
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  public onMouseMove(info: MouseInfo): void {}
+  public onMouseMove(_info: MouseInfo): void {}
   public onClick(info: MouseInfo): void {
     const shipItemHeight = 80;
     const startY = 150;
