cb941094a36202a3d68c04dd698738a872489937
PilkeySEK <PilkeySEK@gmx.de>
2025-10-27 13:46:39 +0100

    cleanup

--- server/src/net/handler/Handler.ts
+++
@@ -1,9 +0,0 @@
-import { WebSocketMessageType } from "../../../../core/src/types";
-import { SocketMessageData } from "../WebSocketServer";
-
-export abstract class WsMessageHandler<T> {
-  abstract handledType: WebSocketMessageType;
-  public abstract handleMessage(
-    data: SocketMessageData<T>,
-  ): Promise<void | string>;
-}
